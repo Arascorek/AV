919f8367e2cb2f0e6ccbecf07ba37a894ce1f556
Arascor <seb4722@googlemail.com>
2023-11-15 22:34:39 +0100

    eddited blacklist

--- components/regelwerk/Banner.vue
+++ components/regelwerk/Banner.vue
@@ -3,7 +3,7 @@
     class="bg-2 text-white mt-20 rounded-lg p-3 text-center text-2xl max-w-2xl w-full mx-auto lg:max-w-4xl 2xl:max-w-5xl 2xl:text-3xl lg:py-4 2xl:py-5"
   >
     <p>Regelwerk:</p>
-    <p class="text-yellow-500">Grundlage für ein gutes Miteinander</p>
+    <p class="text-yellow-500">trwoj sytatry utes Miteinander</p>
   </div>
 </template>
 
