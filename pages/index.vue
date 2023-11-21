0c4bb5bd82a4cb4a1489d190abe6c6830d87987a
Arascor <seb4722@googlemail.com>
2023-11-21 20:59:19 +0100

    startseite

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,16 @@
     <NuxtImg
       class="absolute w-full h-[85vh] object-cover brightness-[25%]"
       src="https://cdn.mos.cms.futurecdn.net/aDp3J8JC4dYFDxqpbCikVL-1200-80.jpg"
-      alt="background image"
     />
     <div class="px-4 container mx-auto relative z-20 text-white">
       <div class="max-w-lg pt-20 lg:pt-24 2xl:pt-32">
         <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Plan B Roleplay</h1>
         <p class="text-md mt-2 mb-10 2xl:text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nulla ad
-          aliquid dolores, veritatis quam tenetur praesentium totam
-          reprehenderit placeat, voluptas adipisci fuga sunt dicta omnis cumque
-          hic?
+          Starte dein Leben in der verzauberten Welt von Plan B. Die Stadt von wahren Roleplay. 
+          Komm aus dir heraus und beschreite dein Weg, als Krimineller oder als vorbildlichen Bürger. 
+          Zudem sind wir Anfängerfreundlich. Jeder Spieler ist willkommen, ob Pro oder Neuling.
         </p>
-        <SharedButton text="Explore the World!" @click="handleScrollWindow" />
+        <SharedButton text="Erkunde die Welt!" @click="handleScrollWindow" />
       </div>
 
       <div
@@ -23,7 +21,7 @@
         <HomeCard
           text="Philosophie"
           imageLink="https://images.hindustantimes.com/tech/img/2023/03/06/960x540/gta_v_1678076345536_1678076352712_1678076352712.png"
-          linkTo="/philosophie"
+          linkTo="/test"
         />
         <HomeCard
           text="Regelwerk"
@@ -38,7 +36,7 @@
         <HomeCard
           text="Discord"
           imageLink="https://www.pockettactics.com/wp-content/sites/pockettactics/2023/02/gta-v-mobile-1-550x309.jpg"
-          linkTo="/regelwerk"
+          linkTo="https://discord.gg/as3VhHMU6a"
         />
       </div>
 
@@ -46,54 +44,47 @@
         class="mt-16 flex flex-col items-center lg:items-start lg:flex-row justify-between container mx-auto pt-20 lg:pt-24 2xl:pt-32"
       >
         <div class="max-w-xl text-center lg:text-left">
-          <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Plan B Roleplay</h1>
+          <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Bad Fraktion</h1>
           <p class="max-w-sm text-md mt-2 mb-10 lg:max-w-lg 2xl:text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nulla
-            ad aliquid dolores, veritatis quam tenetur praesentium totam
-            reprehenderit placeat, voluptas adipisci fuga sunt dicta omnis
-            cumque hic?
+            Bei Plan B kannst du den Weg als Bad Fraktion beschreiten. 
+            Als Gang, Kartell, Mafia o.ä. Kommt aus euch heraus und zeigt der Stadt aus welchen Holz ihr Geschnitzt seit.
           </p>
         </div>
-        <LazyNuxtImg
+        <NuxtImg
           class="max-w-xs lg:max-w-md rounded-lg"
           src="https://images.sftcdn.net/images/t_app-cover-l,f_auto/p/b3d17a4e-96bf-11e6-a0d8-00163ed833e7/1317752825/gta-v-how-to-play-grand-theft-auto-v-windows.jpg"
-          alt="background image"
         />
       </div>
       <div
         class="flex flex-col items-center lg:items-start lg:flex-row-reverse justify-between container mx-auto pt-20 lg:pt-24 2xl:pt-32"
       >
         <div class="max-w-xl text-center lg:text-left">
-          <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Plan B Roleplay</h1>
+          <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Staatsfraktion</h1>
           <p class="max-w-sm text-md mt-2 mb-10 lg:max-w-lg 2xl:text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nulla
-            ad aliquid dolores, veritatis quam tenetur praesentium totam
-            reprehenderit placeat, voluptas adipisci fuga sunt dicta omnis
-            cumque hic?
+            Bei Plan B kannst du den Weg als Staatsfraktion beschreiten. 
+            Werde ein Teil der Stadt. Versorge die Bürger oder beschütze die Bürger. 
+            Lass die Stadt von dir profitieren.
           </p>
         </div>
-        <LazyNuxtImg
+        <NuxtImg
           class="max-w-xs lg:max-w-md rounded-lg"
           src="https://media-rockstargames-com.akamaized.net/tina-uploads/posts/ak73k92o47ko75/5de9d8bc4ffeabb209ec67ab3721ea281da5cd05.jpg"
-          alt="background image"
         />
       </div>
       <div
         class="flex flex-col items-center lg:items-start lg:flex-row justify-between container mx-auto pt-20 lg:pt-24 2xl:pt-32"
       >
         <div class="max-w-xl text-center lg:text-left">
-          <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Plan B Roleplay</h1>
+          <h1 class="text-5xl lg:text-6xl 2xl:text-7xl">Zivi Leben</h1>
           <p class="max-w-sm text-md mt-2 mb-10 lg:max-w-lg 2xl:text-lg">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nulla
-            ad aliquid dolores, veritatis quam tenetur praesentium totam
-            reprehenderit placeat, voluptas adipisci fuga sunt dicta omnis
-            cumque hic?
+            Bei Plan B kannst du den Weg als Zivilist beschreiten. 
+            Entdecke dein eigenen Roleplay  Charakter, mit deiner eigener Art, die du gerne ausspielen möchtest. 
+            Zeig der Plan B Community was du drauf hast.
           </p>
         </div>
-        <LazyNuxtImg
+        <NuxtImg
           class="max-w-xs lg:max-w-md rounded-lg"
           src="https://media-rockstargames-com.akamaized.net/tina-uploads/posts/ak73k92o47ko75/5de9d8bc4ffeabb209ec67ab3721ea281da5cd05.jpg"
-          alt="background image"
         />
       </div>
     </div>
