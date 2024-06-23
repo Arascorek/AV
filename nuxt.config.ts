97816748762af265223b8e78a4ce5ba19af34db4
Arascorek <150952961+Arascorek@users.noreply.github.com>
2024-06-23 21:36:57 +0200

    nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,27 @@
-import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-  // Twoja konfiguracja Nuxt.js tutaj
   modules: [
     '@nuxt/image',
     '@nuxtjs/i18n',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts'
   ],
-
-  // Konfiguracja i18n
   i18n: {
     locales: [
-      { code: 'en', iso: 'en-US', file: 'en.json' },
-      { code: 'pl', iso: 'pl-PL', file: 'pl.json' },
-      { code: 'de', iso: 'de-DE', file: 'de.json' }
+      { code: 'en', name: 'English', iso: 'en-US', file: 'en.json' },
+      { code: 'pl', name: 'Polski', iso: 'pl-PL', file: 'pl.json' },
+      { code: 'de', name: 'Deutsch', iso: 'de-DE', file: 'de.json' }
     ],
-    defaultLocale: 'en',
     lazy: true,
-    langDir: 'lang'
+    langDir: 'lang',
+    defaultLocale: 'en'
   },
-
-  // Konfiguracja Google Fonts
   googleFonts: {
     families: {
       Oswald: true,
     }
   },
-
-  // Dodatkowe style CSS
   css: ['~/assets/css/main.scss'],
-
-  // Konfiguracja PostCSS
   build: {
     postcss: {
       plugins: {
@@ -40,7 +30,5 @@ export default defineNuxtConfig({
       },
     },
   },
-
-  // Lista pluginów
   plugins: ['~/plugins/i18n.js']
 });
