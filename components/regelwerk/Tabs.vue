7e6959b6a9b83a7fb39a1e4ae62f0a41891da489
Arascorek <150952961+Arascorek@users.noreply.github.com>
2024-06-24 19:31:59 +0200

    tabs

--- components/regelwerk/Tabs.vue
+++ components/regelwerk/Tabs.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-[#212121] px-4 pb-4">
-    <div class="flex border-b-4 border-b-yellow-500 max-w-2xl mx-auto lg:max-w-4xl 2xl:max-w-5xl">
+    <div
+      class="flex border-b-4 border-b-yellow-500 max-w-2xl mx-auto lg:max-w-4xl 2xl:max-w-5xl"
+    >
       <RegelwerkTab
         v-for="item in regelwerkRules"
         :key="item.label"
@@ -10,20 +12,17 @@
       />
     </div>
     <RegelwerkTabText
-      :content="regelwerkRules[currentTabIndex.value].content"
-      :title="regelwerkRules[currentTabIndex.value].label"
-      :currentTabIndex="currentTabIndex.value + 1"
+      :text="regelwerkRules[currentTabIndex].content"
+      :title="regelwerkRules[currentTabIndex].label"
+      :currentTabIndex="currentTabIndex + 1"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
 import { regelwerkRules } from '../../data/regulamin';
-import RegelwerkTab from './Tab.vue';
-import RegelwerkTabText from './TabText.vue';
 
-const currentTab = ref<string>('Poland');
+const currentTab = ref<string>('Allgemeines');
 const currentTabIndex = ref<number>(0);
 
 const updateLabel = (newValue: string) => {
