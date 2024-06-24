1b248797f807e29081aaefc526fde3cc526b31bf
Arascorek <150952961+Arascorek@users.noreply.github.com>
2024-06-24 19:14:20 +0200

    ssss

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -12,11 +12,6 @@
       <SharedBanner :title="mainRegelwerkData.bannerWhiteText" :desc="mainRegelwerkData.bannerYellowText" />
     </div>
     <RegelwerkTabs class="z-10 relative min-h-screen" />
-    <!-- Dodane -->
-    <section id="polska" class="projects-section">
-      <h2>POLSKA</h2>
-      <ProjectVillaMare />
-    </section>
   </div>
 </template>
 
@@ -29,18 +24,12 @@ useSeoMeta({
 useHead({
   meta: [{name: 'keywords', content: pageKeyWords.regelwerk}]
 })
-// Importowanie komponentu
-import ProjectVillaMare from '../../components/ProjectVillaMare.vue';
 </script>
-
 <style>
 .bg {
   background-image: url('/public/image11.png');
   background-size: cover;
   background-position: 65% center;
   background-attachment: local;
-}
-.projects-section {
-  margin: 20px 0;
 }
 </style>
